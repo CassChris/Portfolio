a7a65e81fd3b4fc68ac7ac9d643b7895d257fc5c
CassChris <christiancasafrancarocha@gmail.com>
2023-01-07 17:44:48 -0500

    añadiendo estructura y estilos del home

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/header/Header'
-
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
 
 function App() {
-
   return (
     <div className="App">
       <Header />
+      <main className="main">
+        <Home />
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/christiancasafranca/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-instagram-alt"></i>
+      </a>
+      <a
+        href="https://github.com/CassChris"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-github"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/christian-casafranca/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-linkedin"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
