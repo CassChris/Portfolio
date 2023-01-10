3ba57eb862810ca5871d406cd23107bb0b57f685
CassChris <christiancasafrancarocha@gmail.com>
2023-01-10 15:29:28 -0500

    agregando seccion services

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import ParticlesBackground from './components/ParticlesBackground';
+import ParticlesBackground from "./components/ParticlesBackground";
 import "./App.css";
 import About from "./components/about/About";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Skills from "./components/skills/Skills";
+import Services from "./components/services/Services";
 
 function App() {
   return (
     <div className="App">
+        <ParticlesBackground/>
       <Header />
       <main className="main">
         <Home />
-        <About />
-    <ParticlesBackground>
-        <Skills />
-    </ParticlesBackground>
+          <About />
+          <Skills />
+          <Services />
+        
       </main>
     </div>
   );
